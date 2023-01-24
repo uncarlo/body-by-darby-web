22c393b6e68188ddc1be3c958476a12e88245f06
Carlo Toribio <carlo.toribio@gmail.com>
2023-01-23 20:46:38 -0800

    Finished responsive website.

--- src/components/ServiceItem/ServiceItem.js
+++ src/components/ServiceItem/ServiceItem.js
@@ -6,6 +6,7 @@ class ServiceItem extends Component {
 
     render() {
         return <>
+        <div className="service-item-wrapper">
             <div className="service-item">
                 <div className="image">
                     <img src={this.props.imgSrc} alt="item-img" />
@@ -15,12 +16,15 @@ class ServiceItem extends Component {
                     <p className="pricing">{this.props.pricing}</p>
                     <p className="info">{this.props.additionalInfo}</p>
                     <p className="description">{this.props.description}</p>
-                    <a href={this.props.learnMore} >
-                        <button>Learn More...</button>
-                    </a>
+                    <div className="button-container">
+                        <a href={this.props.learnMore} >
+                            <button>Learn More...</button>
+                        </a>
+                    </div>
                 </div>
             </div>
             <div className='separator'></div>
+        </div>
         </>;
     }
 }
